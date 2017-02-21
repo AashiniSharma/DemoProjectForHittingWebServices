fb6363004e7c986b0192a3fd1812e052c6d37a31
Appinventiv <aashinisharma18@gmail.com>
2017-02-21 12:38:53 +0530

    InitialStart

---
+++ SampleView/DemoProjectForHittingServices.swift
@@ -0,0 +1,27 @@
+//
+//  DemoProjectForHittingServices.swift
+//  SampleView
+//
+//  Created by Appinventiv on 21/02/17.
+//  Copyright © 2017 Appinventiv. All rights reserved.
+//
+
+import UIKit
+
+class DemoProjectForHittingServices: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+}
